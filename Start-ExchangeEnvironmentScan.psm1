15a4879c82369ec6fbc9c6c65001f275d0cd51f9
slyxyllt <liu_xin@aliyun.com>
2019-08-20 01:06:52 +0800

    syntax error fixed

--- Start-ExchangeEnvironmentScan.psm1
+++ Start-ExchangeEnvironmentScan.psm1
@@ -1,9 +1,9 @@
 $script:MainPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 $script:FuntionPath = Join-Path -Parent $script:MainPath -Path functions 
-$Functions = @( Get-ChildItem -Path $script:FuntionPath -Inclue *.ps1 -ErrorAction SilentlyContinue )
+$Functions = @( Get-ChildItem -Path $script:FuntionPath -Include *.ps1 -ErrorAction SilentlyContinue )
 Function Start-ExchangeEnvironmentScan {
     param(
         
     )
-    
+
 }
